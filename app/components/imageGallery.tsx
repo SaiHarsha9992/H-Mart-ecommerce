00a88db2fcea47b2bb8d830fae8181e599b4cc1d
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-13 21:52:34 +0530

    Update imageGallery.tsx

--- app/components/imageGallery.tsx
+++ app/components/imageGallery.tsx
@@ -2,17 +2,19 @@
 import Image from "next/image";
 import { urlFor } from "../lib/sanity";
 import { useState } from "react";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 interface iAppProps {
-  images: any;
+  images: SanityImageSource[];
 }
 
 export default function ImageGallery({ images }: iAppProps) {
-  const [bigImage, setBigImage] = useState(images[0]);
+  const [bigImage, setBigImage] = useState<SanityImageSource>(images[0]);
 
-  const handleSmallImageClick = (image: any) => {
+  const handleSmallImageClick = (image: SanityImageSource) => {
     setBigImage(image);
   };
+
   if (!images || images.length === 0) {
     return <p>No images available</p>;
   }
@@ -20,7 +22,7 @@ export default function ImageGallery({ images }: iAppProps) {
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
-        {images.map((image: any, idx: any) => (
+        {images.map((image, idx) => (
           <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
             <Image
               src={urlFor(image).url()}
