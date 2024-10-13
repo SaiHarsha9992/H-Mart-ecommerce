a4b31b17fc891f27ef89591961aaf1247d03ca6c
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2024-10-13 21:32:47 +0530

    first Commit

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -8,7 +8,7 @@ export default function CartProvider({ children }: { children: ReactNode }) {
     <USCProvider
       mode="payment"
       cartMode="client-only"
-      stripe="pk_test_51Q9MQn2LE9Oi7HHjyDN3SucYK2iVuwcQQbMqq1zp8saz9P7R32c3nI1p1VVTw5ObNn5DMfGmKuc6ZjrTPQoIX4wQ00a9MIHLHL"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string}
       successUrl="http://localhost:3000/stripe/success"
       cancelUrl="http://localhost:3000/stripe/cancel"
       currency="INR"
