04c0a3df22fdd1bdf89473b47a56f50cfaed8434
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-13 21:52:08 +0530

    Update interface.ts

--- app/interface.ts
+++ app/interface.ts
@@ -1,3 +1,5 @@
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
 export interface simplifiedProduct {
   _id: string;
   imageUrl: string;
@@ -13,7 +15,7 @@ export interface fullProduct {
   price: number;
   description: string;
   slug: string;
-  image: any;
+  image: SanityImageSource; // Replace 'any' with the appropriate type
   categoryName: string;
   price_id: string;
 }
