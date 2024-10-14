ca37cafae2eaf7af75ab0984e74cf997c093c241
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-14 14:07:21 +0530

    Update sanity.ts

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,15 +1,17 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";  // Import the correct type
 
 export const client = createClient({
   projectId: "5jwdnvws",
   dataset: "production",
+  useCdn: true,
   apiVersion: "2022-03-25",
-  useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: any) {
+// Replace 'any' with 'SanityImageSource'
+export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
