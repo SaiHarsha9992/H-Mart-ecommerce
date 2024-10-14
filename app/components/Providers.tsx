bd3360ea6ed226e515b0f7b774ce447e3a90d1f0
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-14 12:12:54 +0530

    Update Providers.tsx

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe="pk_test_51Q9MQn2LE9Oi7HHjyDN3SucYK2iVuwcQQbMqq1zp8saz9P7R32c3nI1p1VVTw5ObNn5DMfGmKuc6ZjrTPQoIX4wQ00a9MIHLHL"
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/cancel"
+      successUrl="http://h-mart-ecommerce.vercel.app/stripe/success"
+      cancelUrl="http://h-mart-ecommerce.vercel.app/stripe/cancel"
       currency="INR"
       billingAddressCollection={false}
       language="en-US"
