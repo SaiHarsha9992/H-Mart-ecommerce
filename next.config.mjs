9e1910f9b53ed914add84f7ae6ee87326bf68b66
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-14 13:32:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,17 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Applies to all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate", // Prevents caching
+          },
+        ],
+      },
+    ];
+  },
 };
