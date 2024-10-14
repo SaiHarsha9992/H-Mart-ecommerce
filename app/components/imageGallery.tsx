73f1ea462fe31e7f2630ff1510b979db3850443a
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-14 13:44:23 +0530

    Update imageGallery.tsx

--- app/components/imageGallery.tsx
+++ app/components/imageGallery.tsx
@@ -21,14 +21,15 @@ export default function ImageGallery({ images }: iAppProps) {
 
   return (
     <div className="grid gap-4 lg:grid-cols-5">
+      {/* Thumbnail images */}
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
         {images.map((image, idx) => (
           <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
             <Image
-              src={urlFor(image).url()}
-              alt="Product Image"
+              src={urlFor(image).url()} // Generate image URL
+              alt={`Product Image ${idx + 1}`} // Improve alt text for accessibility
               className="w-full h-full object-center object-cover cursor-pointer"
-              width={200}
+              width={200} // Control the thumbnail size
               height={200}
               onClick={() => handleSmallImageClick(image)}
             />
@@ -36,15 +37,16 @@ export default function ImageGallery({ images }: iAppProps) {
         ))}
       </div>
 
+      {/* Main large image */}
       <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
         <Image
-          src={urlFor(bigImage).url()}
-          alt="Photo"
+          src={urlFor(bigImage).url()} // URL for big image
+          alt="Main Product Photo"
           width={500}
           height={500}
           className="h-full w-full object-cover object-center"
         />
-
+        {/* Sale Tag */}
         <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">
           Sale
         </span>
